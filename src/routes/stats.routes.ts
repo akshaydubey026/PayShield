b30dcec15a446772c27a0715862ed9920dc17247
akshaydubey430 <akshaykumardubey023@gmail.com>
2026-04-19 06:00:31 +0530

    Final cleanup: removed duplicate root files

--- src/routes/stats.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as statsController from "../controllers/stats.controller.js";
-
-const statsRouter = Router();
-
-statsRouter.get("/donor-summary", statsController.donorSummary);
-statsRouter.get("/overview", statsController.overview);
-
-export default statsRouter;
